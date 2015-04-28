023f57fe871ccc91e50074a85fd6e4d61da3c6f4
Nicolás Zuasti <nicozuasti@gmail.com>
2015-04-28 10:13:04 -0300

    initial commit

---
+++ app/components/fill-seagal.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  height: 100,
+  width: 100,
+  src: Ember.computed('height', 'width', function() {
+    var base = "http://www.stevensegallery.com/";    
+    return base + this.get('width') + "/" + this.get('height');
+  })
+});
